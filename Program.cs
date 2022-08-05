c061bee8e63f6758b53a7ba219bcdd092e17548a
Tyler Gardner <tyler.gardner@maersk.com>
2022-08-05 10:41:29 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{ 
+
+     public static void Main(string[] args)
+    {
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        double num3 = Convert.ToDouble(Console.ReadLine());
+        double num4 = Convert.ToDouble(Console.ReadLine());
+
+        double result = (num1 + num2 + num3 + num3 + num4) / 4;
+        
+        Console.WriteLine($"The result is: {result}");
+    }
+}
