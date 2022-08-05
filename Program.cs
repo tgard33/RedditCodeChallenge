12b60850a13cadf72b24249d7d81feb7578da71d
Tyler Gardner <tyler.gardner@maersk.com>
2022-08-05 11:48:11 -0600

    Best Solution Added

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿public class Program
-{ 
+{
 
-     public static void Main(string[] args)
+    public static void Main(string[] args)
     {
         //Original Code
         //double num1 = Convert.ToDouble(Console.ReadLine());
@@ -23,20 +23,58 @@
         //double num4 = Convert.ToDouble(Console.ReadLine());
 
         //double result = (num1 + num2 + num3 + num4) / 4;
-        
+
         //Console.WriteLine($"The result is: {result}");
         //Console.ReadKey();
 
 
         //Better Solution
-        List<int> nums = new List<int> {1, 2, 3, 4, 5};
-        //take user input and convert to int
-        nums.Add(Convert.ToInt32(Console.ReadLine()));
+        List<double> nums = new();
+        int input = 0;
+
+        //take n inputs
+        Console.WriteLine("provide # of times to run");
+
+        bool run = true;
+        while (run == true)
+        {
+            try
+            {
+                input = Convert.ToInt32(Console.ReadLine());
+                run = false;
+            }
+            catch
+            {
+                Console.WriteLine("input invalid, try again with a number");
+            }
+        }
+        Console.WriteLine($"begin inputting numbers {input} times");
+        int count = 0;
+        while (count != input)
+        {
+            try
+            {
+                Console.Write(count + 1 + ":");
+                count++;
+                nums.Add(Convert.ToDouble(Console.ReadLine()));
+            }
+            catch
+            {
+                count--;
+                Console.WriteLine($"You input something that isn't an integer, you idiot");
+            }
+        }
+
+        //account for decimals in a double
         double averages = 0;
+
+        //loop each item in list
         foreach (int num in nums)
         {
             averages += num;
         }
-        Console.WriteLine(averages / nums.Count);
+        Console.WriteLine($"The average of your {input} inputs, is {averages / nums.Count}");
+
     }
 }
+    
