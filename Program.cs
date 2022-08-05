0a75ee82cacfa398e39c8412088647efbf00b691
Tyler Gardner <tyler.gardner@maersk.com>
2022-08-05 11:18:46 -0600

    Better solution with input option

--- Program.cs
+++ Program.cs
@@ -16,21 +16,22 @@
 
 
 
-        //Fixed Code and solution
-        double num1 = Convert.ToDouble(Console.ReadLine());
-        double num2 = Convert.ToDouble(Console.ReadLine());
-        double num3 = Convert.ToDouble(Console.ReadLine());
-        double num4 = Convert.ToDouble(Console.ReadLine());
+        ////Fixed Code and solution
+        //double num1 = Convert.ToDouble(Console.ReadLine());
+        //double num2 = Convert.ToDouble(Console.ReadLine());
+        //double num3 = Convert.ToDouble(Console.ReadLine());
+        //double num4 = Convert.ToDouble(Console.ReadLine());
 
-        double result = (num1 + num2 + num3 + num4) / 4;
+        //double result = (num1 + num2 + num3 + num4) / 4;
         
-        Console.WriteLine($"The result is: {result}");
-        Console.ReadKey();
+        //Console.WriteLine($"The result is: {result}");
+        //Console.ReadKey();
 
 
         //Better Solution
-        List<int> nums = new List<int> { 1, 2, 3, 4, 5 };
-
+        List<int> nums = new List<int> {1, 2, 3, 4, 5};
+        //take user input and convert to int
+        nums.Add(Convert.ToInt32(Console.ReadLine()));
         double averages = 0;
         foreach (int num in nums)
         {
